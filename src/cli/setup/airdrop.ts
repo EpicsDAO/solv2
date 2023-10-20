9acfff2f52e13a569af4a14b11dd80980668d13d
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-10-20 09:58:35 +0200

    update: release v2.0.11

--- src/cli/setup/airdrop.ts
+++ src/cli/setup/airdrop.ts
@@ -1,9 +1,19 @@
+import chalk from 'chalk'
 import { spawnSync } from 'child_process'
 
 export const airdrop = () => {
   try {
     const cmd = `solana airdrop 1`
-    spawnSync(cmd, { shell: true, stdio: 'inherit' })
+    const result = String(spawnSync(cmd, { shell: true, stdio: 'inherit' }))
+    if (result.includes('Error')) {
+      console.log(
+        chalk.yellow(
+          `Airdrop failed. Please get 1 SOL and Try Again with this command;\n`
+        )
+      )
+      console.log(chalk.green(`$ solv setup --vote\n`))
+    }
+    return true
   } catch (error) {
     throw new Error(`airdrop Error: ${error}`)
   }
